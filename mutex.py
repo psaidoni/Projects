fe9258ded4c5b776334477b935fdd540d1314f3f
pamela <pamela@oem-VirtualBox>
2018-09-26 15:19:49 -0400

    seconf commit

--- mutex.py
+++ mutex.py
@@ -1,8 +1,10 @@
 from threading import Thread, Lock
 
-mutex = Lock()
+mutex = Lock() 
 CAN_message_1= [0x01, 0x40, 0x00, 0x00, 0x00]
 print(CAN_message_1)
+
+#Decodes the value of the can bus 
 def CANDecode():
   mutex.acquire() #locking the resource
   CAN_message_1[0]=CAN_message_1[0] & 0x3f
@@ -10,6 +12,7 @@ def CANDecode():
   print(decoded)
   mutex.release() #releasing the resource
 
+#Upadate the second value by adding 10
 def CANUpdate():
   mutex.acquire() #locking the resource
   CAN_message_1[1] = CAN_message_1[1]+10
